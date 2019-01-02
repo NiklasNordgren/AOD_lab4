e8769d4b439b5b27da1d69f9a1360eb3ba54d59b
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2019-01-02 17:31:40 +0100

    Commit number: 1

---
+++ src/Launcher.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		
+		
+		arrayList.add(5);
+		arrayList.add(4);
+		arrayList.add(1337);
+		arrayList.add(1338);
+		arrayList.add(9);
+		arrayList.add(30);
+		arrayList.add(50);
+		arrayList.add(1);
+		arrayList.add(2);
+		arrayList.add(99999999);
+		
+		for (Integer integer : arrayList) {
+			System.out.println(integer);
+		}
+		
+		Sorter.quickSort(arrayList);
+		
+		System.out.println("After sort:");
+		for (Integer integer : arrayList) {
+			System.out.println(integer);
+		}
+		
+	}
+
+}
---
+++ src/Sorter.java
@@ -0,0 +1,57 @@
+import java.util.List;
+
+public class Sorter<T> {
+
+	public static <T extends Comparable<? super T>> void quickSort(List<T> list) {
+
+		quickSorter(list, 0, list.size() - 1);
+	}
+
+	private static <T extends Comparable<? super T>> void quickSorter(List<T> list, int low, int high) {
+
+		T pivotValue = medianOfThree(list.get(low), list.get(low + (high - low) / 2), list.get(high));
+
+		int left = low;
+		int right = high;
+
+		while(left <= right) {
+			while(list.get(left).compareTo(pivotValue) < 0) {
+				left++;
+			}
+			while(list.get(right).compareTo(pivotValue) > 0) {
+				right--;
+			}
+			if(left <= right) {
+				swap(list, left, right);
+				left++;
+				right--;
+			}
+		}
+		if(low < right) {
+			quickSorter(list, low, right);
+		}
+		if(left < high) {
+			quickSorter(list, left, high);
+		}
+	}
+
+	private static <T extends Comparable<? super T>> void swap(List<T> list, int a, int b) {
+
+		T temp = list.get(a);
+		list.set(a, list.get(b));
+		list.set(b, temp);
+
+	}
+
+	private static <T extends Comparable<? super T>> T medianOfThree(T first, T middle, T last) {
+
+		if(first.compareTo(last) >= 0 && first.compareTo(middle) >= 0)
+			return first;
+		else if(last.compareTo(first) >= 0 && last.compareTo(middle) >= 0)
+			return last;
+		else
+			return middle;
+
+	}
+
+}
