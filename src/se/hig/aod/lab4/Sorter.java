1d07bbd658a502e42cc44e1e0a0e4e6304c2bb0e
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2019-01-03 21:17:36 +0100

    Added JDoc

--- src/se/hig/aod/lab4/Sorter.java
+++ src/se/hig/aod/lab4/Sorter.java
@@ -2,8 +2,27 @@ package se.hig.aod.lab4;
 
 import java.util.List;
 
+/**
+ * The {@code Sorter} class contains one single public static method {@code quickSort} that performs a Quicksort
+ * on lists containing objects of the type comparable or has a superclass that is of the comparable type.
+ * 
+ * The strategy used to choose pivot element in the Quicksort algorithm is median of three.
+ * 
+ * @author Thomas Lundgren, Niklas Nordgren
+ * @version 1.0.0
+ * @since 2019-01-03
+ */
 public class Sorter {
 
+	/**
+	 * This method performs a Quicksort on a list object containing <T> generic
+	 * type objects of the type comparable or has a superclass that is of the comparable type.
+	 * 
+	 * The strategy used to choose pivot element in the Quicksort algorithm is median of three.
+	 *
+	 * @param <T> the generic type
+	 * @param list
+	 */
 	public static <T extends Comparable<? super T>> void quickSort(List<T> list) {
 		
 		if(list.size() <= 1)
@@ -12,6 +31,14 @@ public class Sorter {
 		quickSorter(list, 0, list.size() - 1);
 	}
 
+	/**
+	 * Quick sorter.
+	 *
+	 * @param <T> the generic type
+	 * @param list the list
+	 * @param low the index of the leftmost element
+	 * @param high the index of the rightmost element
+	 */
 	private static <T extends Comparable<? super T>> void quickSorter(List<T> list, int low, int high) {
 
 		T pivotValue = medianOfThree(list.get(low), list.get(low + (high - low) / 2), list.get(high));
@@ -47,6 +74,14 @@ public class Sorter {
 		
 	}
 
+	/**
+	 * Swap places in the list of the elements at index a and b
+	 *
+	 * @param <T> the generic type
+	 * @param list the list
+	 * @param a index of element a
+	 * @param b index of element b
+	 */
 	private static <T extends Comparable<? super T>> void swap(List<T> list, int a, int b) {
 
 		T temp = list.get(a);
@@ -55,6 +90,15 @@ public class Sorter {
 
 	}
 
+	/**
+	 * Median of three.
+	 *
+	 * @param <T> the generic type
+	 * @param first the leftmost element
+	 * @param middle the middle element
+	 * @param last the rightmost element
+	 * @return median of the first, middle and last element
+	 */
 	private static <T extends Comparable<? super T>> T medianOfThree(T first, T middle, T last) {
 
 		if(first.compareTo(last) >= 0 && first.compareTo(middle) <= 0)
